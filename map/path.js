57b6c08f7269b477269f454f4ceed3ca03cfa075
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-17 10:07:01 +0900

    Add drawPath function to render a path on canvas

--- map/path.js
+++ map/path.js
@@ -7,3 +7,18 @@ export const path = [
   { x: 600, y: 500 },
   { x: 800, y: 500 }
 ];
+// /map/path.js に追加
+import { ctx } from "../core/canvas.js";
+
+export function drawPath() {
+  ctx.strokeStyle = "rgba(0,0,0,0.3)";
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(path[0].x, path[0].y);
+
+  for (let i = 1; i < path.length; i++) {
+    ctx.lineTo(path[i].x, path[i].y);
+  }
+
+  ctx.stroke();
+}
